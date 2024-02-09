603345f33f6c07584949fa045fbe54daa92b418e
kimdongmin <meena2003@naver.com>
2024-02-09 15:33:12 +0900

    feat: 프로젝트 생성

---
+++ src/main/java/dormitoryfamily/doomz/DoomzApplication.java
@@ -0,0 +1,13 @@
+package dormitoryfamily.doomz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoomzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoomzApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dormitoryfamily/doomz/DoomzApplicationTests.java
@@ -0,0 +1,13 @@
+package dormitoryfamily.doomz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoomzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
