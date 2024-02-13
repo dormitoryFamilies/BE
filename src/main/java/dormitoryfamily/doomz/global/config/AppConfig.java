7f47ce4c162015e099828eef5a71c9ba0d47fcfb
kimdongmin <meena2003@naver.com>
2024-02-14 00:28:29 +0900

    setting: 스프링 시큐리티 설정 삭제

--- src/main/java/dormitoryfamily/doomz/global/config/AppConfig.java
+++ src/main/java/dormitoryfamily/doomz/global/config/AppConfig.java
@@ -5,15 +5,9 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 public class AppConfig {
-
-    @Bean
-    public BCryptPasswordEncoder bCryptPasswordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
 
     @Bean
     public ObjectMapper objectMapper() {
