72faf106d8d3e48b5b06e10d8e6a56f7445d4e97
kimdongmin <meena2003@naver.com>
2024-02-16 17:58:03 +0900

    feat: 게시글 조회를 위한 Member 엔티티 필드 추가 및 MemberDormitoryType 생성

---
+++ src/main/java/dormitoryfamily/doomz/domain/member/entity/type/MemberDormitoryType.java
@@ -0,0 +1,23 @@
+package dormitoryfamily.doomz.domain.member.entity.type;
+
+import lombok.Getter;
+
+@Getter
+public enum MemberDormitoryType {
+    JINRIGWAN("진리관"),
+    JEONGUIGWAN("정의관"),
+    GAECHEOKGWAN("개척관"),
+    GYEYOUNGWON("계영원"),
+    JISEONGWAN("지선관"),
+    MYEONGDEOKGWAN("명덕관"),
+    SINMINGWAN("신민관"),
+    INUIGWAN("인의관"),
+    YEJIGWAN("예지관"),
+    DEUNGYONGGWAN("등용관");
+
+    private final String name;
+
+    MemberDormitoryType(String name) {
+        this.name = name;
+    }
+}
