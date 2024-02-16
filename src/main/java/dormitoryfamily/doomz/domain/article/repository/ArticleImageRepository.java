57605bc90d3cf123d5362c5d1da8d455307b3b7d
kimdongmin <meena2003@naver.com>
2024-02-16 13:55:53 +0900

    feat: ArticleImage 엔티티 및 리포지토리 생성

---
+++ src/main/java/dormitoryfamily/doomz/domain/article/repository/ArticleImageRepository.java
@@ -0,0 +1,7 @@
+package dormitoryfamily.doomz.domain.article.repository;
+
+import dormitoryfamily.doomz.domain.article.entity.ArticleImage;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ArticleImageRepository extends JpaRepository<ArticleImage, Long> {
+}
