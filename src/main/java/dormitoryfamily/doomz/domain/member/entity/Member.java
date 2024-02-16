171d4659e5a65fd7a65208d6215835fae34d88d0
kimdongmin <meena2003@naver.com>
2024-02-16 17:58:03 +0900

    feat: 게시글 조회를 위한 Member 엔티티 필드 추가 및 MemberDormitoryType 생성

--- src/main/java/dormitoryfamily/doomz/domain/member/entity/Member.java
+++ src/main/java/dormitoryfamily/doomz/domain/member/entity/Member.java
@@ -1,9 +1,12 @@
 package dormitoryfamily.doomz.domain.member.entity;
 
+import dormitoryfamily.doomz.domain.member.entity.type.MemberDormitoryType;
 import jakarta.persistence.*;
+import lombok.Getter;
 import lombok.Setter;
 
 @Entity
+@Getter
 @Setter
 public class Member {
 
@@ -11,4 +14,10 @@ public class Member {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "member_id")
     private Long id;
+
+    private String nickname;
+    private String profileUrl;
+
+    @Enumerated(EnumType.STRING)
+    private MemberDormitoryType dormitoryType;
 }
