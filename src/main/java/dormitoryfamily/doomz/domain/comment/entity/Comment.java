7188ddfeabfbcc530d460d7d7a4c36007b66d4e3
Somin Park <ps4708@naver.com>
2024-02-17 14:23:12 +0900

    feat: 댓글 등록 기능 구현
    1차적으로 댓글 등록 기능 구현

---
+++ src/main/java/dormitoryfamily/doomz/domain/comment/entity/Comment.java
@@ -0,0 +1,46 @@
+package dormitoryfamily.doomz.domain.comment.entity;
+
+import dormitoryfamily.doomz.domain.article.entity.Article;
+import dormitoryfamily.doomz.domain.member.entity.Member;
+import dormitoryfamily.doomz.global.entity.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import static jakarta.persistence.FetchType.LAZY;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "comment")
+public class Comment extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "comment_id")
+    private Long id;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "article_id")
+    private Article article;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @Column(nullable = false)
+    private String content;
+
+    private boolean isDeleted;
+
+    @Builder
+    public Comment(Article article, Member member, String content, boolean isDeleted) {
+        this.article = article;
+        this.member = member;
+        this.content = content;
+        this.isDeleted = isDeleted;
+    }
+
+}
