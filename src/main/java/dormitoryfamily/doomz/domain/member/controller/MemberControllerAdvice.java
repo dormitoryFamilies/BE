08724e6624a24b67036679b4b4f1093efdec6b4e
kimdongmin <meena2003@naver.com>
2024-02-19 17:48:09 +0900

    feat: 다른 사용자의 게시글을 수정, 삭제 시도할 시 발생하는 예외 추가

---
+++ src/main/java/dormitoryfamily/doomz/domain/member/controller/MemberControllerAdvice.java
@@ -0,0 +1,21 @@
+package dormitoryfamily.doomz.domain.member.controller;
+
+import dormitoryfamily.doomz.domain.member.exception.InvalidMemberAccessException;
+import dormitoryfamily.doomz.global.util.ResponseDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class MemberControllerAdvice {
+
+    @ExceptionHandler
+    public ResponseEntity<ResponseDto<Void>> handleInvalidMemberAccessException(InvalidMemberAccessException e) {
+        HttpStatus status = e.getErrorCode().getHttpStatus();
+
+        return ResponseEntity
+                .status(status)
+                .body(ResponseDto.errorWithMessage(status, e.getMessage()));
+    }
+}
