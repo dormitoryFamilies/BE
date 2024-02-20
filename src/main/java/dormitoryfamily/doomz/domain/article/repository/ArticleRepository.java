7cb04d89dbcffac58604292a3bc8665de138bc0e
kimdongmin <meena2003@naver.com>
2024-02-20 23:21:54 +0900

    feat: 기숙사별 게시글 목록 조회 1차 구현
    paging, 정렬 구현은 아직 못했음.

--- src/main/java/dormitoryfamily/doomz/domain/article/repository/ArticleRepository.java
+++ src/main/java/dormitoryfamily/doomz/domain/article/repository/ArticleRepository.java
@@ -1,7 +1,14 @@
 package dormitoryfamily.doomz.domain.article.repository;
 
 import dormitoryfamily.doomz.domain.article.entity.Article;
+import dormitoryfamily.doomz.domain.article.entity.type.ArticleDormitoryType;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface ArticleRepository extends JpaRepository<Article, Long> {
+
+    @Query("SELECT a FROM Article a WHERE a.dormitoryType = :dormitoryType")
+    Slice<Article> findByArticleDormitoryType(@Param("dormitoryType") ArticleDormitoryType dormitoryType);
 }
