17ba9b285ede3fddab806c12a85c1510a598f89c
Somin Park <ps4708@naver.com>
2024-02-22 02:37:44 +0900

    refactor: 대댓글 패키지 코드 일괄 정리

--- src/main/java/dormitoryfamily/doomz/domain/replyComment/repository/ReplyCommentRepository.java
+++ src/main/java/dormitoryfamily/doomz/domain/replyComment/repository/ReplyCommentRepository.java
@@ -3,9 +3,7 @@ package dormitoryfamily.doomz.domain.replyComment.repository;
 import dormitoryfamily.doomz.domain.replyComment.entity.ReplyComment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface ReplyCommentRepository extends JpaRepository<ReplyComment, Long> {
 
-    Optional<ReplyComment> findFirstByCommentId(Long commentId);
+    boolean existsByCommentId(Long commentId);
 }
