119f6548a57d05338841ac11cd2325c1e7fd2f78
Somin Park <ps4708@naver.com>
2024-02-21 11:07:04 +0900

    fix: 대댓글이 없는 삭제된 댓글의 경우 삭제 처리 해결
    related #13

--- src/main/java/dormitoryfamily/doomz/domain/replyComment/repository/ReplyCommentRepository.java
+++ src/main/java/dormitoryfamily/doomz/domain/replyComment/repository/ReplyCommentRepository.java
@@ -3,5 +3,9 @@ package dormitoryfamily.doomz.domain.replyComment.repository;
 import dormitoryfamily.doomz.domain.replyComment.entity.ReplyComment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ReplyCommentRepository extends JpaRepository<ReplyComment, Long> {
+
+    Optional<ReplyComment> findFirstByCommentId(Long commentId);
 }
