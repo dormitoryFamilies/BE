4bfb1eebcd026aa100e4d8232ddb60cd82400a33
kimdongmin <meena2003@naver.com>
2024-02-22 15:28:49 +0900

    feat: 기숙사별 게시글 리스트 조회 기능 1차 구현
    1차적으로 기능을 구현 완료.

---
+++ src/main/java/dormitoryfamily/doomz/domain/article/repository/ArticleRepositoryCustom.java
@@ -0,0 +1,13 @@
+package dormitoryfamily.doomz.domain.article.repository;
+
+import dormitoryfamily.doomz.domain.article.dto.request.ArticleRequest;
+import dormitoryfamily.doomz.domain.article.entity.Article;
+import dormitoryfamily.doomz.domain.article.entity.type.ArticleDormitoryType;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+
+public interface ArticleRepositoryCustom {
+
+//    @EntityGraph(attributePaths = "member")
+    Slice<Article> findAllByDormitoryType(ArticleDormitoryType dormitoryType, ArticleRequest request, Pageable pageable);
+}
