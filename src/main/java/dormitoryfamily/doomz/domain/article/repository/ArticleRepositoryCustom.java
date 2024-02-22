0ccdcd0a3f662f33d1de7f715334606c605394ab
Somin Park <ps4708@naver.com>
2024-02-22 17:21:11 +0900

    게시글 검색 기능 1차 구현
    related #25

--- src/main/java/dormitoryfamily/doomz/domain/article/repository/ArticleRepositoryCustom.java
+++ src/main/java/dormitoryfamily/doomz/domain/article/repository/ArticleRepositoryCustom.java
@@ -3,11 +3,12 @@ package dormitoryfamily.doomz.domain.article.repository;
 import dormitoryfamily.doomz.domain.article.dto.request.ArticleRequest;
 import dormitoryfamily.doomz.domain.article.entity.Article;
 import dormitoryfamily.doomz.domain.article.entity.type.ArticleDormitoryType;
-import dormitoryfamily.doomz.domain.article.entity.type.BoardType;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 
 public interface ArticleRepositoryCustom {
 
-    Slice<Article> findAllByDormitoryTypeAndBoardType(ArticleDormitoryType dormitoryType, BoardType boardType, ArticleRequest request, Pageable pageable);
+    //    @EntityGraph(attributePaths = "member")
+    Slice<Article> findAllByDormitoryType(ArticleDormitoryType dormitoryType, ArticleRequest request, Pageable pageable);
+    Slice<Article> searchArticles(ArticleDormitoryType dormitoryType, String keyword, Pageable pageable);
 }
