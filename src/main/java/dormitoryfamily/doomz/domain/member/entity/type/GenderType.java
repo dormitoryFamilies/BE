86b0b3f1dfc8c5219e45ca884697d79d37a20c7a
Somin Park <ps4708@naver.com>
2024-02-25 16:09:41 +0900

    feat: 성별 enum 생성

--- src/main/java/dormitoryfamily/doomz/domain/member/entity/type/GenderType.java
+++ src/main/java/dormitoryfamily/doomz/domain/member/entity/type/GenderType.java
@@ -1,7 +1,27 @@
 package dormitoryfamily.doomz.domain.member.entity.type;
 
+import dormitoryfamily.doomz.domain.article.exception.InvalidStatusTypeException;
+import dormitoryfamily.doomz.domain.member.exception.InvalidGenderTypeException;
 import lombok.Getter;
 
 @Getter
 public enum GenderType {
+
+    MALE("남자"),
+    FEMALE("여자");
+
+    private final String description;
+
+    GenderType(String description) {
+        this.description = description;
+    }
+
+    public static GenderType fromDescription(String description) {
+        for (GenderType type : GenderType.values()) {
+            if (type.description.equals(description)) {
+                return type;
+            }
+        }
+        throw new InvalidGenderTypeException();
+    }
 }
