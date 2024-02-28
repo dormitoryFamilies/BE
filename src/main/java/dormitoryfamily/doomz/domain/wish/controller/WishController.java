a3fd04431622a42e9a94d43b7304b582b4f86789
Somin Park <ps4708@naver.com>
2024-02-28 20:23:44 +0900

    feat: 내가 찜한 게시글 목록 조회 기능 추가

--- src/main/java/dormitoryfamily/doomz/domain/wish/controller/WishController.java
+++ src/main/java/dormitoryfamily/doomz/domain/wish/controller/WishController.java
@@ -1,21 +1,25 @@
 package dormitoryfamily.doomz.domain.wish.controller;
 
+import dormitoryfamily.doomz.domain.article.dto.response.ArticleListResponseDto;
 import dormitoryfamily.doomz.domain.member.entity.Member;
 import dormitoryfamily.doomz.domain.wish.dto.WishMemberListResponseDto;
 import dormitoryfamily.doomz.domain.wish.service.WishService;
 import dormitoryfamily.doomz.global.util.ResponseDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+
+
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/articles/{articleId}/wishes")
+@RequestMapping("/api")
 public class WishController {
 
     private final WishService wishService;
 
-    @PostMapping
+    @PostMapping("/articles/{articleId}/wishes")
     public ResponseEntity<ResponseDto<Void>> saveWish(
             @PathVariable Long articleId
     ){
@@ -27,7 +31,7 @@ public class WishController {
         return ResponseEntity.ok(ResponseDto.created());
     }
 
-    @GetMapping
+    @GetMapping("/articles/{articleId}/wishes")
     public ResponseEntity<ResponseDto<WishMemberListResponseDto>> getWishMemberList(
             @PathVariable Long articleId
     ){
@@ -35,7 +39,7 @@ public class WishController {
         return ResponseEntity.ok(ResponseDto.okWithData(responseDto));
     }
 
-    @DeleteMapping
+    @DeleteMapping("/articles/{articleId}/wishes")
     public ResponseEntity<ResponseDto<Void>> cancelWish(
             @PathVariable Long articleId
     ){
@@ -45,5 +49,18 @@ public class WishController {
 
         wishService.removeWish(member, articleId);
         return ResponseEntity.ok(ResponseDto.ok());
+    }
+
+    @GetMapping("/mypage/articleWishes")
+    public ResponseEntity<ResponseDto<ArticleListResponseDto>> findMyArticleWishes(
+            @RequestParam String dormitoryType,
+            Pageable pageable
+    ){
+        //삭제 예정
+        Member member = new Member();
+        member.setId(1L);
+
+        ArticleListResponseDto responseDto = wishService.findMyArticleWishes(member, dormitoryType, pageable);
+        return ResponseEntity.ok(ResponseDto.okWithData(responseDto));
     }
 }
