aca24d569cd68ba9fbbd6708e790631c059b9314
Somin Park <ps4708@naver.com>
2024-02-29 01:49:32 +0900

    feat: follow 엔티티 생성

---
+++ src/main/java/dormitoryfamily/doomz/domain/follow/entity/Follow.java
@@ -0,0 +1,29 @@
+package dormitoryfamily.doomz.domain.follow.entity;
+
+import dormitoryfamily.doomz.domain.member.entity.Member;
+import dormitoryfamily.doomz.global.entity.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import static jakarta.persistence.FetchType.LAZY;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Follow extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "follow_id")
+    private Long id;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "follower_id")
+    private Member follower;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "following_id")
+    private Member following;
+}
