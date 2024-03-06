c8ff61946e4e38378c3ab76976c8fccccabdbb82
Somin Park <ps4708@naver.com>
2024-03-07 00:19:40 +0900

    fix: 배포 시 docker-compose.yml 파일을 찾지 못하는 오류 해결

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,11 +2,13 @@
 
 APP_NAME=doomz
 
+cd /home/ubuntu/app
+
 echo "> Stop and remove existing containers"
 docker-compose down
 
 echo "> Remove previous Docker image"
-docker rmi "$APP_NAME"
+docker rmi -f "$APP_NAME"
 
 echo "> Build and run Docker containers using Docker Compose"
 docker-compose up -d
