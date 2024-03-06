ad9653417610bf42831bdbb638a01d75d13dc65e
Somin Park <ps4708@naver.com>
2024-03-06 23:26:10 +0900

    feat: 배포 시 redis 관련 코드 추가

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,25 +2,11 @@
 
 APP_NAME=doomz
 
-echo "> Check the currently running container"
-CONTAINER_ID=$(docker ps -aqf "name=$APP_NAME")
-
-if [ -z "$CONTAINER_ID" ];
-then
-  echo "> No such container is running."
-else
-  echo "> Stop and remove container: $CONTAINER_ID"
-  docker stop "$CONTAINER_ID"
-  docker rm "$CONTAINER_ID"
-fi
+echo "> Stop and remove existing containers"
+docker-compose down
 
 echo "> Remove previous Docker image"
 docker rmi "$APP_NAME"
 
-cd /home/ubuntu/app
-
-echo "> Build Docker image"
-docker build -t "$APP_NAME" .
-
-echo "> Run the Docker container"
-docker run -d -p 8080:8080 --name "$APP_NAME" "$APP_NAME"
+echo "> Build and run Docker containers using Docker Compose"
+docker-compose up -d
