834044767f04b699e649dd57bc3d4232d4e9ce5e
Somin Park <ps4708@naver.com>
2024-03-08 00:12:11 +0900

    remove: redis 연동 테스트 코드 삭제

--- src/main/java/dormitoryfamily/doomz/domain/redisTest/PersonRedisRepository.java
+++
@@ -1,8 +0,0 @@
-package dormitoryfamily.doomz.domain.redisTest;
-
-import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
-import org.springframework.data.repository.CrudRepository;
-
-@EnableRedisRepositories
-public interface PersonRedisRepository extends CrudRepository<Person, String> {
-}
