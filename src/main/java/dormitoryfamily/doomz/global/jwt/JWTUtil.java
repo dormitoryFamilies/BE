24ef020bb5cda22688ae1f2f845cca4c9a7a7ffd
kimdongmin <meena2003@naver.com>
2024-03-09 23:25:42 +0900

    feat: JWT 기능 생성

---
+++ src/main/java/dormitoryfamily/doomz/global/jwt/JWTUtil.java
@@ -0,0 +1,48 @@
+package dormitoryfamily.doomz.global.jwt;
+
+import dormitoryfamily.doomz.domain.member.entity.Member;
+import io.jsonwebtoken.Jwts;
+import org.springframework.data.util.Pair;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+
+@Component
+public class JWTUtil {
+
+    public String createToken(Member member) {
+        Pair<String, Key> key = JWTKey.getRandomKey();
+
+        return Jwts.builder()
+                .claim("email", member.getEmail())
+//                .claim("role", member.getAuthority())
+                .issuedAt(new Date(System.currentTimeMillis())) // 토큰 발생 시간
+                .expiration(new Date(System.currentTimeMillis() + JWTProperties.ACCESS_TOKEN_EXPIRATION_TIME)) // 토큰 만료 시간
+                .header()
+                .keyId(key.getFirst()) //kid 설정
+                .and()
+                .signWith(key.getSecond()) // signature
+                .compact();
+    }
+
+//    public Boolean isExpired(String token) {
+//        return Jwts.parser()
+//                .setSigningKeyResolver(SigningKeyResolver.instance)
+//                .build()
+//                .parseSignedClaims(token)
+//                .getPayload()
+//                .getExpiration()
+//                .before(new Date());
+//    }
+
+    public String getEmail(String token) {
+        // jwtToken에서 email을 찾습니다.
+        return Jwts.parser()
+                .setSigningKeyResolver(SigningKeyResolver.instance)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload()
+                .getSubject();
+    }
+}
---
+++ src/main/java/dormitoryfamily/doomz/global/jwt/SigningKeyResolver.java
@@ -0,0 +1,20 @@
+package dormitoryfamily.doomz.global.jwt;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwsHeader;
+import io.jsonwebtoken.SigningKeyResolverAdapter;
+
+import java.security.Key;
+
+public class SigningKeyResolver extends SigningKeyResolverAdapter {
+
+    public static SigningKeyResolver instance = new SigningKeyResolver();
+
+    @Override
+    public Key resolveSigningKey(JwsHeader jwsHeader, Claims claims) {
+        String kid = jwsHeader.getKeyId();
+        if (kid == null)
+            return null;
+        return JWTKey.getKey(kid);
+    }
+}
