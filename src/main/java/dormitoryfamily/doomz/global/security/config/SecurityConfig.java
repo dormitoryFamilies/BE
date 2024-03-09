b356debabbd6590caa56ccea546bf40dbda39ba7
kimdongmin <meena2003@naver.com>
2024-03-09 23:26:11 +0900

    feat: 시큐리티 config 생성

--- src/main/java/dormitoryfamily/doomz/global/security/config/SecurityConfig.java
+++ src/main/java/dormitoryfamily/doomz/global/security/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package dormitoryfamily.doomz.global.security.config;
 
+import dormitoryfamily.doomz.global.jwt.JWTAuthorizationFilter;
+import dormitoryfamily.doomz.global.oauth2.OAuth2LoginSuccessHandler;
 import dormitoryfamily.doomz.global.oauth2.OAuth2UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
@@ -7,7 +9,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.client.web.OAuth2LoginAuthenticationFilter;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
@@ -15,27 +22,59 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
     private final OAuth2UserService oAuth2UserService;
+    private final JWTAuthorizationFilter jwtAuthorizationFilter;
+    private final OAuth2LoginSuccessHandler oAuth2LoginSuccessHandler;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .csrf(AbstractHttpConfigurer::disable);
 
-        http
-                .formLogin(AbstractHttpConfigurer::disable);
+        // CORS 설정 추가
+        http.cors(cors -> cors
+                .configurationSource(request -> {
+                    CorsConfiguration configuration = new CorsConfiguration();
+                    configuration.applyPermitDefaultValues();
+                    configuration.addAllowedOriginPattern("");
+                    configuration.addAllowedOriginPattern("http://localhost:3000");
+                    configuration.addAllowedOriginPattern("http://43.202.254.127:8080/");
+                    configuration.setAllowedMethods(
+                            Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "HEAD"));
 
-        http
-                .httpBasic(AbstractHttpConfigurer::disable);
+                    // 다른 도메인도 필요에 따라 추가
+                    configuration.setAllowCredentials(true); // 쿠키를 포함한 크로스 도메인 요청을 허용
+                    return configuration;
+                }));
 
-        http
-                .oauth2Login((oauth2) -> oauth2
-                        .userInfoEndpoint(userInfoEndpointConfig ->
-                                userInfoEndpointConfig.userService(oAuth2UserService)));
+        // HTTP Basic 인증 방식 비설정
+        http.httpBasic(AbstractHttpConfigurer::disable);
 
-        http
-                .authorizeHttpRequests((auth) -> auth
-                        .requestMatchers("/", "/oauth2/**", "/login").permitAll()
-                        .anyRequest().authenticated());
+        // csrf 비설정
+        http.csrf(AbstractHttpConfigurer::disable);
+
+        // Form 로그인 방식 비설정
+        http.formLogin(AbstractHttpConfigurer::disable);
+
+        // rememberme 비설정
+        http.rememberMe(AbstractHttpConfigurer::disable);
+
+        // JWT 필터 추가
+//        http.addFilterBefore(jwtAuthorizationFilter, UsernamePasswordAuthenticationFilter.class);
+        http.addFilterAfter(jwtAuthorizationFilter, OAuth2LoginAuthenticationFilter.class);
+
+        // 경로별 인가 작업
+        http.authorizeHttpRequests((auth) -> auth
+                .requestMatchers("/").permitAll()
+                .anyRequest().authenticated());
+
+        //oauth2 로그인
+        http.oauth2Login((oauth2) -> oauth2
+                .userInfoEndpoint(userInfoEndpointConfig -> userInfoEndpointConfig.userService(oAuth2UserService))
+                .successHandler(oAuth2LoginSuccessHandler)
+                .defaultSuccessUrl("/"));
+
+
+        // 세션 stateless 설정
+        http.sessionManagement((session) -> session
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
         return http.build();
     }
