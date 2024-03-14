56f3cb1aeb534e258a314b9b7e9330a8e2970387
kimdongmin <meena2003@naver.com>
2024-03-14 18:47:36 +0900

    fix: 게시글이 없는 예외 상황에 대한 에러 코드를 400에서 404로 변경

--- src/main/java/dormitoryfamily/doomz/global/exception/ErrorCode.java
+++ src/main/java/dormitoryfamily/doomz/global/exception/ErrorCode.java
@@ -16,7 +16,7 @@ public enum ErrorCode {
     ARTICLE_DORMITORY_TYPE_NOT_EXISTS(HttpStatus.BAD_REQUEST, "해당 기숙사 유형이 존재하지 않습니다."),
     BOARD_TYPE_NOT_EXISTS(HttpStatus.BAD_REQUEST, "해당 게시판 유형이 존재하지 않습니다."),
     STATUS_TYPE_NOT_EXISTS(HttpStatus.BAD_REQUEST, "해당 게시글 상태는 존재하지 않습니다"),
-    ARTICLE_NOT_EXISTS(HttpStatus.BAD_REQUEST, "해당 게시글은 존재하지 않습니다."),
+    ARTICLE_NOT_EXISTS(HttpStatus.NOT_FOUND, "해당 게시글은 존재하지 않습니다."),
     STATUS_ALREADY_SET(HttpStatus.BAD_REQUEST, null),
 
     // Member
