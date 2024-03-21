ea3e214fdda117dc8cb3141f85dbc8b1e7cdce97
kimdongmin <meena2003@naver.com>
2024-03-21 22:51:27 +0900

    setting: JWT 만료 시간 1억분으로 변경

--- src/main/java/dormitoryfamily/doomz/global/jwt/JWTProperties.java
+++ src/main/java/dormitoryfamily/doomz/global/jwt/JWTProperties.java
@@ -2,5 +2,5 @@ package dormitoryfamily.doomz.global.jwt;
 
 public class JWTProperties {
 
-    public static final int ACCESS_TOKEN_EXPIRATION_TIME = 100 * 60 * 60 * 40; //10분
+    public static final int ACCESS_TOKEN_EXPIRATION_TIME = 60000 * 10000; // 1분 * 10000 =
 }
