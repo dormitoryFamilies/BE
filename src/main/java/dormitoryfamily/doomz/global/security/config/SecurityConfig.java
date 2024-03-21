2973027850a871d7f83cbdb98a7d8ce358b6d0fa
kimdongmin <meena2003@naver.com>
2024-03-22 00:20:41 +0900

    fix: 프론트 CORS 설정 변경

--- src/main/java/dormitoryfamily/doomz/global/security/config/SecurityConfig.java
+++ src/main/java/dormitoryfamily/doomz/global/security/config/SecurityConfig.java
@@ -64,6 +64,7 @@ public class SecurityConfig {
         // 경로별 인가 작업
         http.authorizeHttpRequests((auth) -> auth
                 .requestMatchers("/").permitAll()
+                .requestMatchers(CorsUtils::isPreFlightRequest).permitAll()
                 .requestMatchers("/api/reissue").permitAll()
                 .requestMatchers("/","/stomp/**").permitAll()
                 .requestMatchers(CorsUtils::isPreFlightRequest).permitAll()
