664dcb589de81e7c2df4344d907e3840c5242a0f
kimdongmin <meena2003@naver.com>
2024-03-26 23:23:14 +0900

    feat: Refresh 토큰을 이용하여 Access 토큰 재발급 요청 컨트롤러 및 서비스 1차 구현

---
+++ src/main/java/dormitoryfamily/doomz/global/jwt/ReissueController.java
@@ -0,0 +1,22 @@
+package dormitoryfamily.doomz.global.jwt;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api")
+public class ReissueController {
+
+    private final ReissueService reissueService;
+
+    @PostMapping("/reissue")
+    public ResponseEntity<?> reissue(HttpServletRequest request, HttpServletResponse response) {
+        return reissueService.reissue(request, response);
+    }
+}
