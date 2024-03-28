bdc58b59d84632a34c33a3ab59f0cd6adebedb0c
kimdongmin <meena2003@naver.com>
2024-03-28 18:09:09 +0900

    setting: Access 토큰 만료시간 30분, Refresh 토큰 만료시간 24시간 설정

--- src/main/java/dormitoryfamily/doomz/global/jwt/JWTProperties.java
+++ src/main/java/dormitoryfamily/doomz/global/jwt/JWTProperties.java
@@ -7,7 +7,6 @@ public class JWTProperties {
     public static final String HEADER_STRING_REFRESH = "RefreshToken";
     public static final String CATEGORY_ACCESS = "access";
     public static final String CATEGORY_REFRESH = "refresh";
-    //    public static final int ACCESS_TOKEN_EXPIRATION_TIME = 60000 * 10000; // 1분 * 10000 =
-    public static final int ACCESS_TOKEN_EXPIRATION_TIME = 60000; // 1분 * 10000 =
-    public static final int REFRESH_TOKEN_EXPIRATION_TIME = 60000 * 5; // 5분
+    public static final int ACCESS_TOKEN_EXPIRATION_TIME = 60000 * 30; // 30분
+    public static final int REFRESH_TOKEN_EXPIRATION_TIME = 60000 * 60 * 24; // 24시간
 }
