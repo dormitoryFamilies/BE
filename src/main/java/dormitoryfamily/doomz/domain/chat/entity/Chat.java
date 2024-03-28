910409317b5c86db836299602c07db52c840c8aa
Somin Park <ps4708@naver.com>
2024-03-28 15:25:39 +0900

    feat: 채팅과 채팅룽 연관관계 매핑

--- src/main/java/dormitoryfamily/doomz/domain/chat/entity/Chat.java
+++ src/main/java/dormitoryfamily/doomz/domain/chat/entity/Chat.java
@@ -1,6 +1,7 @@
 package dormitoryfamily.doomz.domain.chat.entity;
 
 import dormitoryfamily.doomz.domain.chat.entity.type.VisibleStatus;
+import dormitoryfamily.doomz.domain.chatRoom.entity.ChatRoom;
 import dormitoryfamily.doomz.global.entity.BaseTimeEntity;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
@@ -31,5 +32,9 @@ public class Chat extends BaseTimeEntity {
 
     @Enumerated(EnumType.STRING)
     private VisibleStatus visible;
+
+    @ManyToOne
+    @JoinColumn(name = "chat_room_Id", referencedColumnName = "room_uuid", insertable = false, updatable = false)
+    private ChatRoom chatRoom;
 
 }
