ddaa1e4a93e5db30b5ba3527a9ef92d822363bd7
Somin Park <ps4708@naver.com>
2024-04-21 00:48:37 +0900

    remove: redis에 채팅방 정보 저장 코드 제거
    redis에 채팅방 정보를 저장할 필요 x

--- src/main/java/dormitoryfamily/doomz/global/config/RedisConfig.java
+++ src/main/java/dormitoryfamily/doomz/global/config/RedisConfig.java
@@ -1,4 +1,4 @@
-package dormitoryfamily.doomz.global.chat;
+package dormitoryfamily.doomz.global.config;
 
 import dormitoryfamily.doomz.domain.chat.dto.ChatDto;
 import org.springframework.beans.factory.annotation.Value;
