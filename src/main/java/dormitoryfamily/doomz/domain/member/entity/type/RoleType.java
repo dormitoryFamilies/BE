88a024921f83fc1eb89c8e628732201fe055169f
kimdongmin <meena2003@naver.com>
2024-04-28 18:06:19 +0900

    feat: 유저의 권한(Role)을 네 분류로 구분 및 JWT에 반영

---
+++ src/main/java/dormitoryfamily/doomz/domain/member/entity/type/RoleType.java
@@ -0,0 +1,13 @@
+package dormitoryfamily.doomz.domain.member.entity.type;
+
+public enum RoleType {
+
+    ROLE_VISITOR, // 회원가입 이전 유저
+    ROLE_MEMBER, // 프로필 설정 완료 유저
+    ROLE_VERIFIED_STUDENT, // 학생증 인증까지 완료한 유저
+    ROLE_ADMIN; // 관리자
+
+    private RoleType() {
+
+    }
+}
