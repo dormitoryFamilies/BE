7b9493343edd699c462a9256a0fb349a934a963f
Somin Park <ps4708@naver.com>
2024-05-12 18:46:59 +0900

    feat: 마이페이지 나의 프로필 조회 기능 추가

--- src/main/java/dormitoryfamily/doomz/domain/member/controller/MemberController.java
+++ src/main/java/dormitoryfamily/doomz/domain/member/controller/MemberController.java
@@ -1,12 +1,13 @@
 package dormitoryfamily.doomz.domain.member.controller;
 
 import dormitoryfamily.doomz.domain.member.dto.response.MemberProfileResponseDto;
-import dormitoryfamily.doomz.domain.member.entity.Member;
-import dormitoryfamily.doomz.domain.member.repository.MemberRepository;
+import dormitoryfamily.doomz.domain.member.dto.response.MyProfileResponseDto;
 import dormitoryfamily.doomz.domain.member.service.MemberService;
+import dormitoryfamily.doomz.global.security.dto.PrincipalDetails;
 import dormitoryfamily.doomz.global.util.ResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -21,6 +22,14 @@ public class MemberController {
             @PathVariable Long memberId
     ){
         MemberProfileResponseDto responseDto = memberService.getMemberProfile(memberId);
+        return ResponseEntity.ok(ResponseDto.okWithData(responseDto));
+    }
+
+    @GetMapping("/my/profile")
+    public ResponseEntity<ResponseDto<MyProfileResponseDto>> getMyProfile(
+            @AuthenticationPrincipal PrincipalDetails principalDetails
+    ){
+        MyProfileResponseDto responseDto = memberService.getMyProfile(principalDetails);
         return ResponseEntity.ok(ResponseDto.okWithData(responseDto));
     }
 }
--- src/main/java/dormitoryfamily/doomz/domain/member/service/MemberService.java
+++ src/main/java/dormitoryfamily/doomz/domain/member/service/MemberService.java
@@ -1,9 +1,11 @@
 package dormitoryfamily.doomz.domain.member.service;
 
 import dormitoryfamily.doomz.domain.member.dto.response.MemberProfileResponseDto;
+import dormitoryfamily.doomz.domain.member.dto.response.MyProfileResponseDto;
 import dormitoryfamily.doomz.domain.member.entity.Member;
 import dormitoryfamily.doomz.domain.member.exception.MemberNotExistsException;
 import dormitoryfamily.doomz.domain.member.repository.MemberRepository;
+import dormitoryfamily.doomz.global.security.dto.PrincipalDetails;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -23,5 +25,10 @@ public class MemberService {
     private Member getMemberById(Long memberId) {
         return memberRepository.findById(memberId)
                 .orElseThrow(MemberNotExistsException::new);
+    }
+
+    public MyProfileResponseDto getMyProfile(PrincipalDetails principalDetails) {
+        Member loginMember = principalDetails.getMember();
+        return MyProfileResponseDto.fromEntity(loginMember);
     }
 }
