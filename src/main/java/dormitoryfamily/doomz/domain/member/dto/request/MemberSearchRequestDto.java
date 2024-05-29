1500317e61736880112a29f5c630b550b4e3a199
Somin Park <ps4708@naver.com>
2024-05-30 01:19:02 +0900

    refactor: 검색 api에서 사용하는 requestDto를 글로벌 패키지로 변경

--- src/main/java/dormitoryfamily/doomz/domain/member/dto/request/MemberSearchRequestDto.java
+++
@@ -1,9 +0,0 @@
-package dormitoryfamily.doomz.domain.member.dto.request;
-
-import jakarta.validation.constraints.NotBlank;
-
-public record MemberSearchRequestDto (
-        @NotBlank(message = "검색어를 입력해주세요")
-        String q
-){
-}
