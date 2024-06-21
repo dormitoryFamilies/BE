7681092707138b4f54ca1d5e606d749a6d8ee5fa
SoMin Park <92261242+somln@users.noreply.github.com>
2024-06-21 19:35:37 +0900

    Revert "feat: redis에 저장하는 채팅 model명 변경 및 생성"
    This reverts commit 813df13b897abd44b7cd187bfb0a3efac7c4dadb.

--- src/main/java/dormitoryfamily/doomz/global/redis/RedisSubscriber.java
+++ src/main/java/dormitoryfamily/doomz/global/redis/RedisSubscriber.java
@@ -1,6 +1,7 @@
 package dormitoryfamily.doomz.global.redis;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import dormitoryfamily.doomz.domain.chat.dto.ChatDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.connection.Message;
@@ -24,9 +25,9 @@ public class RedisSubscriber implements MessageListener {
 
             String publishMessage = (String) redisTemplate.getStringSerializer().deserialize(message.getBody());
 
-            ChatData chatData = objectMapper.readValue(publishMessage, ChatData.class);
+            ChatDto chatDto = objectMapper.readValue(publishMessage, ChatDto.class);
 
-            messagingTemplate.convertAndSend("/sub/chat/room/" + chatData.roomUUID(), chatData);
+            messagingTemplate.convertAndSend("/sub/chat/room/" + chatDto.roomUUID(), chatDto);
 
         } catch (Exception e) {
             log.error(e.getMessage());
