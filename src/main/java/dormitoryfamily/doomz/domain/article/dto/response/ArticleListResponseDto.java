3e9622322fa378538cbff90771fc42b570b4d8e9
kimdongmin <meena2003@naver.com>
2024-07-02 16:51:13 +0900

    feat: 게시글(Article) 응답 dto에 로그인한 사용자 id 추가

--- src/main/java/dormitoryfamily/doomz/domain/article/dto/response/ArticleListResponseDto.java
+++ src/main/java/dormitoryfamily/doomz/domain/article/dto/response/ArticleListResponseDto.java
@@ -1,20 +1,24 @@
 package dormitoryfamily.doomz.domain.article.dto.response;
 
 import dormitoryfamily.doomz.domain.article.entity.Article;
+import dormitoryfamily.doomz.domain.member.entity.Member;
 import org.springframework.data.domain.Slice;
 
 import java.util.List;
 
 public record ArticleListResponseDto(
-    int nowPageNumber,
-    boolean isLast,
-    List<SimpleArticleResponseDto> articles
+        long loginMemberId,
+        int nowPageNumber,
+        boolean isLast,
+        List<SimpleArticleResponseDto> articles
 ) {
     public static ArticleListResponseDto fromResponseDtos(
+            Member loginMember,
             Slice<Article> articles,
             List<SimpleArticleResponseDto> articleResponseDtos
     ) {
         return new ArticleListResponseDto(
+                loginMember.getId(),
                 articles.getNumber(),
                 articles.isLast(),
                 articleResponseDtos
