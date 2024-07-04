f5fad248a019bb3d8de63044f79e32383c1a9bda
kimdongmin <meena2003@naver.com>
2024-07-04 22:08:10 +0900

    refactor: 게시글, 멤버 기숙사 ErrorCode 변경

---
+++ src/main/java/dormitoryfamily/doomz/domain/article/exception/InvalidArticleDormitoryTypeException.java
@@ -0,0 +1,13 @@
+package dormitoryfamily.doomz.domain.article.exception;
+
+import dormitoryfamily.doomz.global.exception.ApplicationException;
+import dormitoryfamily.doomz.global.exception.ErrorCode;
+
+public class InvalidArticleDormitoryTypeException extends ApplicationException {
+
+    private static final ErrorCode ERROR_CODE = ErrorCode.ARTICLE_DORMITORY_TYPE_NOT_EXISTS;
+
+    public InvalidArticleDormitoryTypeException() {
+        super(ERROR_CODE);
+    }
+}
--- src/main/java/dormitoryfamily/doomz/domain/member/exception/InvalidMemberDormitoryTypeException.java
+++ src/main/java/dormitoryfamily/doomz/domain/member/exception/InvalidMemberDormitoryTypeException.java
@@ -5,7 +5,7 @@ import dormitoryfamily.doomz.global.exception.ErrorCode;
 
 public class InvalidMemberDormitoryTypeException extends ApplicationException {
 
-    private static final ErrorCode ERROR_CODE = ErrorCode.INVALID_MEMBER_DORMITORY_TYPE;
+    private static final ErrorCode ERROR_CODE = ErrorCode.MEMBER_DORMITORY_TYPE_NOT_EXISTS;
 
     public InvalidMemberDormitoryTypeException() {
         super(ERROR_CODE);
