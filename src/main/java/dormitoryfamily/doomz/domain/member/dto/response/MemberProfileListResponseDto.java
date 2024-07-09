942778598030f4670f0213de558d9a9e584d580a
Somin Park <ps4708@naver.com>
2024-07-09 14:45:15 +0900

    refactor: 멤버 프로필 dto MemberProfiles -> memberProfiles

--- src/main/java/dormitoryfamily/doomz/domain/member/dto/response/MemberProfileListResponseDto.java
+++ src/main/java/dormitoryfamily/doomz/domain/member/dto/response/MemberProfileListResponseDto.java
@@ -3,7 +3,7 @@ package dormitoryfamily.doomz.domain.member.dto.response;
 import java.util.List;
 
 public record MemberProfileListResponseDto (
-        List<MemberProfileResponseDto> MemberProfiles
+        List<MemberProfileResponseDto> memberProfiles
 ){
     public static MemberProfileListResponseDto toDto(List<MemberProfileResponseDto> memberProfiles){
         return new MemberProfileListResponseDto(memberProfiles);
