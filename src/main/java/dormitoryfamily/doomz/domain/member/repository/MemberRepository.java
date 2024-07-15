de6140e8daf8fd6fbdf9de007d85ba447c693344
Somin Park <ps4708@naver.com>
2024-07-15 11:03:32 +0900

    feat: 전체 사용자 조회 기능 추가

--- src/main/java/dormitoryfamily/doomz/domain/member/repository/MemberRepository.java
+++ src/main/java/dormitoryfamily/doomz/domain/member/repository/MemberRepository.java
@@ -13,8 +13,14 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
 
     @Query("SELECT m FROM Member m " +
             "WHERE LOWER(m.nickname) LIKE LOWER(CONCAT('%', :keyword, '%')) " +
-            "AND m.id NOT IN (SELECT f.following.id FROM Follow f WHERE f.follower.id = :followerId)")
+            "AND m.id NOT IN (SELECT f.following.id FROM Follow f WHERE f.follower.id = :followerId) " +
+            "ORDER BY m.createdAt DESC")
     List<Member> findMembersExcludingFollowed(Long followerId, String keyword);
+
+    @Query("SELECT m FROM Member m " +
+            "WHERE m.id NOT IN (SELECT f.following.id FROM Follow f WHERE f.follower.id = :followerId) " +
+            "ORDER BY m.createdAt DESC")
+    List<Member> findAllMembersExcludingFollowed(Long followerId);
 
     boolean existsByNickname(String nickname);
 }
