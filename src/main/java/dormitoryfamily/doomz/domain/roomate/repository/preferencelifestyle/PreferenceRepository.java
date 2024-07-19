c990e4061ace2c939c7f6f46f504c55b33f1f714
kimdongmin <meena2003@naver.com>
2024-07-20 04:00:47 +0900

    feat: 라이프 스타일 선호 우선순위 등록 기능 및 예외처리 구현

--- src/main/java/dormitoryfamily/doomz/domain/roomate/repository/preferencelifestyle/PreferenceRepository.java
+++ src/main/java/dormitoryfamily/doomz/domain/roomate/repository/preferencelifestyle/PreferenceRepository.java
@@ -4,4 +4,6 @@ import dormitoryfamily.doomz.domain.roomate.entity.PreferenceLifestyle;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PreferenceRepository extends JpaRepository<PreferenceLifestyle, Long> {
+
+    Boolean existsByMemberId(Long MemberId);
 }
--- src/main/java/dormitoryfamily/doomz/domain/roomate/service/PreferenceLifestyleService.java
+++ src/main/java/dormitoryfamily/doomz/domain/roomate/service/PreferenceLifestyleService.java
@@ -1,34 +1,42 @@
 package dormitoryfamily.doomz.domain.roomate.service;
 
 import dormitoryfamily.doomz.domain.member.entity.Member;
-import dormitoryfamily.doomz.domain.member.repository.MemberRepository;
 import dormitoryfamily.doomz.domain.roomate.dto.preferencelifestyle.request.PreferenceLifestyleRequestDto;
 import dormitoryfamily.doomz.domain.roomate.entity.PreferenceLifestyle;
 import dormitoryfamily.doomz.domain.roomate.entity.type.LifestyleType;
+import dormitoryfamily.doomz.domain.roomate.exception.AlreadyRegisterPreferenceOrderException;
 import dormitoryfamily.doomz.domain.roomate.repository.preferencelifestyle.PreferenceRepository;
 import dormitoryfamily.doomz.global.security.dto.PrincipalDetails;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class PreferenceLifestyleService {
 
-    private final MemberRepository memberRepository;
     private final PreferenceRepository preferenceRepository;
 
     public void savePreferenceLifestyle(PreferenceLifestyleRequestDto requestDto, PrincipalDetails principalDetails) {
-        Member member = memberRepository.findById(principalDetails.getMember().getId()).get();
+        Member loginMember = principalDetails.getMember();
+        checkAlreadySetPreferenceOrder(loginMember);
 
-        savePreference(member, requestDto.firstPreferenceType(), requestDto.firstPreference(), 1);
-        savePreference(member, requestDto.secondPreferenceType(), requestDto.secondPreference(), 2);
-        savePreference(member, requestDto.thirdPreferenceType(), requestDto.thirdPreference(), 3);
-        savePreference(member, requestDto.fourthPreferenceType(), requestDto.fourthPreference(), 4);
+        savePreference(loginMember, requestDto.firstPreferenceType(), requestDto.firstPreference(), 1);
+        savePreference(loginMember, requestDto.secondPreferenceType(), requestDto.secondPreference(), 2);
+        savePreference(loginMember, requestDto.thirdPreferenceType(), requestDto.thirdPreference(), 3);
+        savePreference(loginMember, requestDto.fourthPreferenceType(), requestDto.fourthPreference(), 4);
     }
 
-    private void savePreference(Member member, String preferenceTypeStr, String preferenceStr, int order) {
+    private void savePreference(Member member, String preferenceTypeStr, String preferenceStr, Integer order) {
         LifestyleType preferenceType = LifestyleType.fromType(preferenceTypeStr);
         Enum<?> preferenceDetail = preferenceType.getLifestyleValue(preferenceStr);
         preferenceRepository.save(new PreferenceLifestyle(member, preferenceType, preferenceDetail, order));
+    }
+
+    private void checkAlreadySetPreferenceOrder(Member loginMember) {
+        if (preferenceRepository.existsByMemberId(loginMember.getId())) {
+            throw new AlreadyRegisterPreferenceOrderException();
+        }
     }
 }
