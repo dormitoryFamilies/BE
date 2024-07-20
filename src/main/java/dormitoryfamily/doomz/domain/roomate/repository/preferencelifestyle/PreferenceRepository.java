4de455b01fdb86737db73da1679181bf8773ec5c
kimdongmin <meena2003@naver.com>
2024-07-20 15:08:33 +0900

    feat: 선호 우선순위 등록/수정 요청 API를 통합하기

--- src/main/java/dormitoryfamily/doomz/domain/roomate/repository/preferencelifestyle/PreferenceRepository.java
+++ src/main/java/dormitoryfamily/doomz/domain/roomate/repository/preferencelifestyle/PreferenceRepository.java
@@ -1,9 +1,12 @@
 package dormitoryfamily.doomz.domain.roomate.repository.preferencelifestyle;
 
+import dormitoryfamily.doomz.domain.member.entity.Member;
 import dormitoryfamily.doomz.domain.roomate.entity.PreferenceLifestyle;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PreferenceRepository extends JpaRepository<PreferenceLifestyle, Long> {
 
     Boolean existsByMemberId(Long MemberId);
+
+    void deleteByMember(Member member);
 }
