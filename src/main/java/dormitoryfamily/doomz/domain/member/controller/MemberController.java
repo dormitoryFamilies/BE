40d45d361802594de4e7bf1ac13767500f661f0a
kimdongmin <meena2003@naver.com>
2024-07-22 04:35:45 +0900

    feat: 학생증 인증이 안된 사용자 리스트 조회

--- src/main/java/dormitoryfamily/doomz/domain/member/controller/MemberController.java
+++ src/main/java/dormitoryfamily/doomz/domain/member/controller/MemberController.java
@@ -1,27 +1,22 @@
 package dormitoryfamily.doomz.domain.member.controller;
 
 import dormitoryfamily.doomz.domain.member.dto.request.MemberSetUpProfileRequestDto;
+import dormitoryfamily.doomz.domain.member.dto.request.MyProfileModifyRequestDto;
 import dormitoryfamily.doomz.domain.member.dto.response.*;
 import dormitoryfamily.doomz.domain.member.service.MemberService;
 import dormitoryfamily.doomz.global.security.dto.PrincipalDetails;
 import dormitoryfamily.doomz.global.util.ResponseDto;
+import dormitoryfamily.doomz.global.util.SearchRequestDto;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.RequiredArgsConstructor;
 import org.hibernate.validator.constraints.Length;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.validation.annotation.Validated;
-import dormitoryfamily.doomz.domain.member.dto.request.MyProfileModifyRequestDto;
-import dormitoryfamily.doomz.global.util.SearchRequestDto;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-import dormitoryfamily.doomz.domain.member.dto.response.MemberProfileListResponseDto;
-import dormitoryfamily.doomz.domain.member.dto.response.MyProfileResponseDto;
 
 @RestController
 @RequiredArgsConstructor
@@ -34,7 +29,7 @@ public class MemberController {
     @GetMapping("/members")
     public ResponseEntity<ResponseDto<MemberProfileListResponseDto>> findAllMembers(
             @AuthenticationPrincipal PrincipalDetails principalDetails
-    ){
+    ) {
         MemberProfileListResponseDto responseDto = memberService.findAllMembers(principalDetails);
         return ResponseEntity.ok(ResponseDto.okWithData(responseDto));
     }
@@ -117,5 +112,14 @@ public class MemberController {
         RoommateMatchingMemberProfileResponseDto responseDto =
                 memberService.findRoommateProfile(principalDetails.getMember().getId());
         return ResponseEntity.ok(ResponseDto.okWithData(responseDto));
+    }
+
+    @GetMapping("/verify/student-cards")
+    public ResponseEntity<ResponseDto<NonVerifiedStudentCardsResponseDto>> getStudentCards(
+            Pageable pageable
+    ) {
+        NonVerifiedStudentCardsResponseDto responseDtos =
+                memberService.getNonVerifiedStudentCards(pageable);
+        return ResponseEntity.ok(ResponseDto.okWithData(responseDtos));
     }
 }
