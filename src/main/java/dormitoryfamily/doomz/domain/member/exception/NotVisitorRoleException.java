fb7bc6be851eae5b85419d703c84739aa103c19f
kimdongmin <meena2003@naver.com>
2024-07-22 14:25:51 +0900

    refactor: NOT_VISITOR_ROLE enum 이름을 NOT_ROLE_VISITOR 로 변경

--- src/main/java/dormitoryfamily/doomz/domain/member/exception/NotVisitorRoleException.java
+++ src/main/java/dormitoryfamily/doomz/domain/member/exception/NotVisitorRoleException.java
@@ -5,7 +5,7 @@ import dormitoryfamily.doomz.global.exception.ErrorCode;
 
 public class NotVisitorRoleException extends ApplicationException {
 
-    private static final ErrorCode ERROR_CODE = ErrorCode.NOT_VISITOR_ROLE;
+    private static final ErrorCode ERROR_CODE = ErrorCode.NOT_ROLE_VISITOR;
 
     public NotVisitorRoleException() {
         super(ERROR_CODE);
