742c5cacbe6cdb33969ab41b6573cc8152ff1f95
kimdongmin <meena2003@naver.com>
2024-07-25 23:00:52 +0900

    feat: 기상시간, 샤워시간대 Enum 타입 값 수정

--- src/main/java/dormitoryfamily/doomz/domain/roomate/entity/type/WakeUpTimeType.java
+++ src/main/java/dormitoryfamily/doomz/domain/roomate/entity/type/WakeUpTimeType.java
@@ -6,15 +6,15 @@ import lombok.Getter;
 @Getter
 public enum WakeUpTimeType implements Describable {
 
-    BEFORE_0400("오전 4시 이전"),
-    _0400("오전 4시"),
-    _0500("오전 5시"),
+    BEFORE_0500("오전 5시 이전"),
     _0600("오전 6시"),
     _0700("오전 7시"),
     _0800("오전 8시"),
     _0900("오전 9시"),
     _1000("오전 10시"),
-    AFTER_1000("오전 10시 이후");
+    _1100("오전 11시"),
+    _1200("오후 12시"),
+    AFTER_1300("오후 1시 이후");
 
     private final String description;
 
