ea705d3b408a0cebc740f36776d6b120bde82e89
kimdongmin <meena2003@naver.com>
2024-07-26 11:37:48 +0900

    feat: 룸메 매칭 Recommendation, Candidate 엔티티와 리포지토리 클래스 생성

---
+++ src/main/java/dormitoryfamily/doomz/domain/roomate/entity/Candidate.java
@@ -0,0 +1,39 @@
+package dormitoryfamily.doomz.domain.roomate.entity;
+
+import dormitoryfamily.doomz.domain.member.entity.Member;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import static jakarta.persistence.FetchType.LAZY;
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Candidate {
+
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    @Column(name = "candidate_id")
+    private Long id;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "recommendation_id")
+    private Recommendation recommendation;
+
+    @OneToOne
+    @JoinColumn(name = "candidate_member_id")
+    private Member candidateMember;
+
+    private Integer candidateScore;
+
+    @Builder
+    public Candidate(Recommendation recommendation, Member candidateMember, Integer candidateScore) {
+        this.recommendation = recommendation;
+        this.candidateMember = candidateMember;
+        this.candidateScore = candidateScore;
+    }
+}
