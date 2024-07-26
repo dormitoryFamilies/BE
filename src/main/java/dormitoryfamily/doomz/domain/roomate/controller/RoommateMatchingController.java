2130a2367672efa01e68760c12052ad622c2b7e3
kimdongmin <meena2003@naver.com>
2024-07-26 18:22:12 +0900

    feat: (1차 구현) 선호 우선순위 값과 라이프스타일 값을 비교해 총점 도출 기능

--- src/main/java/dormitoryfamily/doomz/domain/roomate/controller/RoommateMatchingController.java
+++ src/main/java/dormitoryfamily/doomz/domain/roomate/controller/RoommateMatchingController.java
@@ -126,7 +126,7 @@ public class RoommateMatchingController {
     public ResponseEntity<ResponseDto<RecommendationResponseDto>> suggestCandidates(
             @AuthenticationPrincipal PrincipalDetails principalDetails
     ) {
-        RecommendationResponseDto responseDto = recommendationService.suggestRoommates(principalDetails);
+        RecommendationResponseDto responseDto = recommendationService.findTop5Candidates(principalDetails);
         return ResponseEntity.ok(ResponseDto.okWithData(responseDto));
     }
 }
