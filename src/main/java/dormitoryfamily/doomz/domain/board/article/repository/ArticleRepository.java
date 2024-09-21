cb2ea057d907f8cd75fcfe9b2e58231199146da5
kimdongmin <meena2003@naver.com>
2024-09-21 14:07:04 +0900

    refactor: 게시글 단 건, 리스트 조회 시 fetch join 적용

--- src/main/java/dormitoryfamily/doomz/domain/board/article/repository/ArticleRepository.java
+++ src/main/java/dormitoryfamily/doomz/domain/board/article/repository/ArticleRepository.java
@@ -1,7 +1,14 @@
 package dormitoryfamily.doomz.domain.board.article.repository;
 
 import dormitoryfamily.doomz.domain.board.article.entity.Article;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ArticleRepository extends JpaRepository<Article, Long>, ArticleRepositoryCustom {
+
+    @Override
+    @EntityGraph(attributePaths = "member")
+    Optional<Article> findById(Long id);
 }
--- src/main/java/dormitoryfamily/doomz/domain/board/article/repository/ArticleRepositoryCustomImpl.java
+++ src/main/java/dormitoryfamily/doomz/domain/board/article/repository/ArticleRepositoryCustomImpl.java
@@ -33,6 +33,7 @@ public class ArticleRepositoryCustomImpl implements ArticleRepositoryCustom {
 
         List<Article> content = queryFactory
                 .selectFrom(article)
+                .leftJoin(article.member).fetchJoin()
                 .where(
                         dormitoryTypeEq(dormitoryType),
                         articleStatus(request),
