6621d42d7169e1799bb8ffa71eee214748fb71d4
Somin Park <ps4708@naver.com>
2025-04-15 18:52:10 +0900

    feat: opensearch 인증 정보 캐싱 비활성화

--- src/main/java/dormitoryfamily/doomz/global/config/ElasticsearchConfig.java
+++ src/main/java/dormitoryfamily/doomz/global/config/ElasticsearchConfig.java
@@ -4,42 +4,38 @@ import co.elastic.clients.elasticsearch.ElasticsearchClient;
 import co.elastic.clients.json.jackson.JacksonJsonpMapper;
 import co.elastic.clients.transport.ElasticsearchTransport;
 import co.elastic.clients.transport.rest_client.RestClientTransport;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.http.HttpHost;
 import org.apache.http.auth.AuthScope;
 import org.apache.http.auth.UsernamePasswordCredentials;
 import org.apache.http.client.CredentialsProvider;
 import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.message.BasicHeader;
 import org.apache.http.ssl.SSLContextBuilder;
 import org.elasticsearch.client.RestClient;
-import org.elasticsearch.client.RestClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import lombok.extern.slf4j.Slf4j;
+import org.apache.http.HttpResponseInterceptor;
 
 import javax.net.ssl.SSLContext;
-import org.apache.http.message.BasicHeader;
 import java.util.Arrays;
 
 @Slf4j
 @Configuration
 public class ElasticsearchConfig {
 
-    // OpenSearch 엔드포인트 주소
     @Value("${opensearch.endpoint}")
     private String endpoint;
 
-    // OpenSearch 인증 사용자 이름
     @Value("${opensearch.username}")
     private String username;
 
-    // OpenSearch 인증 비밀번호
     @Value("${opensearch.password}")
     private String password;
 
     @Bean
-    public ElasticsearchClient elasticsearchClient() {
-
+    public RestClient restClient() {
         try {
             // SSL 인증서 검증을 비활성화함 (개발 환경에서만 사용할 것)
             SSLContext sslContext = SSLContextBuilder.create()
@@ -52,27 +48,36 @@ public class ElasticsearchConfig {
                     new UsernamePasswordCredentials(username, password));
 
             // RestClientBuilder를 생성하여 인증과 SSL 설정을 적용함
-            RestClientBuilder builder = RestClient.builder(
-                            new HttpHost(endpoint, 443, "https"))
-                    .setHttpClientConfigCallback(httpClientBuilder ->
-                            httpClientBuilder.setDefaultCredentialsProvider(credentialsProvider)
-                                    .setSSLContext(sslContext)
-                                    .setDefaultHeaders(Arrays.asList(
-                                            new BasicHeader("Content-Type", "application/json"),
-                                            new BasicHeader("X-Elastic-Product", "Elasticsearch")
-                                    )));
-
-            // 전송 객체(Transport)를 생성하고 JSON 매퍼로 Jackson을 사용함
-            ElasticsearchTransport transport = new RestClientTransport(
-                    builder.build(), new JacksonJsonpMapper());
-
-            // Elasticsearch 클라이언트를 생성함
-            log.info("✅ OpenSearch 클라이언트 생성 완료");
-            return new ElasticsearchClient(transport);
-
+            return RestClient.builder(new HttpHost(endpoint, 443, "https"))
+                    .setRequestConfigCallback(requestConfigBuilder ->
+                            requestConfigBuilder
+                                    .setConnectTimeout(5000)  // 5초
+                                    .setSocketTimeout(60000)  // 60초
+                    )
+                    .setHttpClientConfigCallback(httpClientBuilder -> {
+                        // disableAuthCaching() 제거함 - 이것이 문제의 원인일 수 있음
+                        httpClientBuilder.setDefaultHeaders(Arrays.asList(
+                                new BasicHeader("Content-Type", "application/json")));
+                        httpClientBuilder.addInterceptorLast((HttpResponseInterceptor)
+                                (response, context) ->
+                                        response.addHeader("X-Elastic-Product", "Elasticsearch"));
+                        return httpClientBuilder.setDefaultCredentialsProvider(credentialsProvider)
+                                .setSSLContext(sslContext);
+                    })
+                    .build();
         } catch (Exception e) {
-            log.error("❌ OpenSearch 클라이언트 생성 실패", e);
-            throw new RuntimeException("OpenSearch 클라이언트 생성 실패", e);
+            log.error("❌ RestClient 생성 실패", e);
+            throw new RuntimeException("RestClient 생성 실패", e);
         }
+    }
+
+    @Bean
+    public ElasticsearchTransport elasticsearchTransport(RestClient restClient) {
+        return new RestClientTransport(restClient, new JacksonJsonpMapper());
+    }
+
+    @Bean
+    public ElasticsearchClient elasticsearchClient(ElasticsearchTransport transport) {
+        return new ElasticsearchClient(transport);
     }
 }
--- src/main/java/dormitoryfamily/doomz/global/controller/DummyDataController.java
+++ src/main/java/dormitoryfamily/doomz/global/controller/DummyDataController.java
@@ -1,9 +1,12 @@
 package dormitoryfamily.doomz.global.controller;
 
 import dormitoryfamily.doomz.global.config.DataInitializer;
+import dormitoryfamily.doomz.global.security.dto.PrincipalDetails;
 import dormitoryfamily.doomz.global.util.ResponseDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Profile;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,7 +18,7 @@ public class DummyDataController {
     private final DataInitializer dataInitializer;
     
     @PostMapping("/dev/dummy-data")
-    public ResponseEntity<ResponseDto<Void>> createDummyData() {
+    public ResponseEntity<ResponseDto<Void>> createDummyData(@AuthenticationPrincipal PrincipalDetails principalDetails) {
         dataInitializer.createAndSaveDummyData();
         return ResponseEntity.ok(ResponseDto.created());
     }
